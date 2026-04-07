d2370563c33d3ce65832a047440c43410044c908
Islam Belal <qobtanislam@gmail.com>
2026-04-07 18:33:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let rev = document.getElementById("rev")
+let btn = document.getElementById("btn")
+btn.onclick = function(){
+    rev.innerHTML= "well done"
+}
+alert(" we are revising ")
+prompt(" oh baby ")
+let smsm = document.getElementById("smsm")
+smsm.onclick = function(){
+  window.print()
+};
